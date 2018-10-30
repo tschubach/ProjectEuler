9d210a6d0c202f1e9d7fcca9d3aab199e6671f95
Tim Schubach <tim.schubach@gmail.com>
2018-10-30 18:20:07 -0400

    Merge conflicts

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -8,11 +8,7 @@ namespace ProjectEuler
         {
             try
             {
-<<<<<<< HEAD
-                Console.WriteLine("Answer: " + RomanNumerals.Problem89());
-=======
                 Console.WriteLine("Answer: \n" + TrinaryTriumph.LargestPrimeFactor(600851475143));
->>>>>>> 3aea7e8d646052b0bc8f6a82a420dc072ef1df91
             }
             catch (Exception e)
             {
