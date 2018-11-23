39e1003bf7d4f4341e7de0abbbaabc985b8203a8
Tim Schubach <TSchubach@geico.com>
2018-11-23 11:24:50 -0500

    cleanup

--- ProjectEuler.Utilities/Helpers.cs
+++ ProjectEuler.Utilities/Helpers.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿import-Module -Name posh-git
+
+Start-SshAgent
+
+cd c:\source\repos
 using System.Numerics;
 
 namespace ProjectEuler.Utilities
