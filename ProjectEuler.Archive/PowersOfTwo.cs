f89aaf694cb994d7cd698c149551f8e834c297b4
Tim Schubach <TSchubach@geico.com>
2019-03-07 15:19:53 -0500

    Changes to PowersOfTwo class

--- ProjectEuler.Archive/PowersOfTwo.cs
+++ ProjectEuler.Archive/PowersOfTwo.cs
@@ -1,36 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Numerics;
 
 namespace ProjectEuler
 {
     public static class PowersOfTwo
     {
-        public static List<int> List(int max)
+        public static IEnumerable<BigInteger> GetPowersOfTwoLessThan(BigInteger max)
         {
-            var listOfPowers = new List<int>();
-            var power = 1;
+            var listOfPowers = new List<BigInteger>();
+            BigInteger power = 1;
 
             while (power < max)
             {
+                listOfPowers.Add(power);
                 power *= 2;
-                if (power < max)
-                {
-                    listOfPowers.Add(power);
-                }
             }
 
             return listOfPowers;
         }
 
-        public static int SumOfDigits(int power)
+        public static BigInteger SumOfDigits(int power)
         {
             int sumOfDigits = 0;
             BigInteger value = BigInteger.Pow(2, power);
 
             while (value > 0)
             {
-                sumOfDigits += (int) (value % 10);
+                sumOfDigits += (int)(value % 10);
                 value /= 10;
             }
 
